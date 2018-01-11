334a10f9b70e2872551d50973f82668400e71370
Ruby Rose <deadsurgeon42@gmail.com>
2018-01-11 23:15:12 +0300

    initial command framework

---
+++ CGGCTF/Core/Commands/CommandException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGGCTF.Core.Commands
+{
+  public class CommandException : Exception
+  {
+    public CommandException(string message)
+    {
+      Message = message;
+    }
+
+    public override string Message { get; }
+  }
+}
